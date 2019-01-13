d92bcaa776ecb05130def026d92770f61bf51eba
Figur8 <ismael_profissional@hotmail.com>
2019-01-13 12:28:57 -0200

    Initial commit

---
+++ complete/src/main/java/hello/Aluno.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+/**
+ *
+ * @author Ismae
+ */
+//Lembrar de sempre usar a porra do asterixo
+@CrossOrigin("*")
+@Entity
+public class Aluno implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    private Integer idAluno;
+    private String curso;
+    private String email;
+    private String nome;
+    private String semestre;
+    private String telefone;
+    private String turno;
+
+    public Aluno() {
+    }
+
+    public Aluno(Integer idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    public Integer getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(Integer idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    
+    
+}
